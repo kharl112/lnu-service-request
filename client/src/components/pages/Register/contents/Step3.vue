32beeeed46a507e8fea5194a81f17702b462824f
kharl112 <kharl.dms112@gmail.com>
2021-05-10 07:21:18 -0500

    step3 of register component created

---
+++ client/src/components/pages/Register/contents/Step3.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: "Step3",
+  data: () => {
+    return {
+      departments: ["IT", "ED", "Tourism", "PE"],
+      roles: [
+        { role: "Member", value: 1 },
+        { role: "Chair Person", value: 2 },
+        { role: "Head", value: 3 },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <v-card class="card-container" elevation="5">
+    <v-form>
+      <v-select
+        class="input"
+        label="Department"
+        :items="departments"
+        autofocus
+        outlined
+        background-color="#E5E5E5"
+        required
+      />
+      <v-select
+        class="input"
+        label="Role/Position"
+        :items="roles"
+        item-text="role"
+        item-value="value"
+        autofocus
+        outlined
+        background-color="#E5E5E5"
+        required
+      />
+      <v-btn type="submit" bottom color="warning" medium>
+        Next
+      </v-btn>
+    </v-form>
+  </v-card>
+</template>
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,6 +1,7 @@
 import Register from "../components/pages/Register/Register";
 import Step1 from "../components/pages/Register/contents/Step1";
 import Step2 from "../components/pages/Register/contents/Step2";
+import Step3 from "../components/pages/Register/contents/Step3";
 import Login from "../components/pages/Login/Login";
 
 export const routes = [
@@ -26,6 +27,7 @@ export const routes = [
     children: [
       { path: "step=1", component: Step1 },
       { path: "step=2", component: Step2 },
+      { path: "step=3", component: Step3 },
     ],
   },
 ];
