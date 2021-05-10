c219aba3d2aef09fe80b86c41569c2529511800c
kharl112 <kharl.dms112@gmail.com>
2021-05-10 00:11:35 -0500

    LeftBg2 created and imported in Login component

--- client/src/components/pages/Login/Login.vue
+++ client/src/components/pages/Login/Login.vue
@@ -1,9 +1,11 @@
 <script>
 import LeftBg1 from "../../svgs/LeftBg1";
+import LeftBg2 from "../../svgs/LeftBg2";
 export default {
   name: "Login",
   components: {
     LeftBg1,
+    LeftBg2,
   },
   computed: {
     getUserType() {
@@ -16,7 +18,8 @@ export default {
   <v-container fluid id="login-container">
     <v-row dense no-gutters id="login-row1">
       <v-col cols="3" id="login-col1">
-        <LeftBg1 />
+        <LeftBg1 v-if="getUserType === 'faculty'" />
+        <LeftBg2 v-else />
       </v-col>
       <v-col cols="9" id="login-col2"> </v-col>
     </v-row>
