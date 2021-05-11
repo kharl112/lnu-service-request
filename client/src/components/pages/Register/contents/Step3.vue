453a2010dd9d76f67bbaaabd4703c9280a1fc9f0
kharl112 <kharl.dms112@gmail.com>
2021-05-10 20:25:07 -0500

    validation in register component step 3 created

--- client/src/components/pages/Register/contents/Step3.vue
+++ client/src/components/pages/Register/contents/Step3.vue
@@ -9,31 +9,40 @@ export default {
         { role: "Chair Person", value: 2 },
         { role: "Head", value: 3 },
       ],
+      rules: {
+        notNull: [(v) => !!v || "This field is required"],
+      },
     };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.$refs.form.validate()) return;
+    },
   },
 };
 </script>
 <template>
   <v-card class="card-container" elevation="5">
-    <v-form>
+    <v-form ref="form" @submit="handleSubmit">
       <v-select
         class="input"
         label="Department"
         :items="departments"
+        :rules="rules.notNull"
         autofocus
         outlined
         background-color="#E5E5E5"
-        required
       />
       <v-select
         class="input"
         label="Role/Position"
         :items="roles"
+        :rules="rules.notNull"
         item-text="role"
         item-value="value"
         outlined
         background-color="#E5E5E5"
-        required
       />
       <v-btn type="submit" bottom color="warning" medium>
         Next
