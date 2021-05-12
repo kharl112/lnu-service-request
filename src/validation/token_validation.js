1efc19963ddd14080c9c2fde02f406544fe738a2
kharl112 <kharl.dms112@gmail.com>
2021-05-12 01:15:54 -0500

    fixed typs in token validation

--- src/validation/token_validation.js
+++ src/validation/token_validation.js
@@ -5,7 +5,7 @@ const create = (body) => {
     creatorID: Joi.string().min(24).max(24).required(),
     claimerID: Joi.string().min(24).max(24).allow(""),
     claimed: Joi.boolean().required(),
-    token: Join.string().min(9).max(9).required(),
+    token: Joi.string().min(9).max(9).required(),
   });
   return schema.validate(body);
 };
