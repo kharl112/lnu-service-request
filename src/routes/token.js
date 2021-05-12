aad4c0ebfe47db680471c4b36ba931c3a063432b
kharl112 <kharl.dms112@gmail.com>
2021-05-12 01:48:49 -0500

    token route>create created

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,15 +1,22 @@
 const route = require("express").Router();
+const jwt = require("jsonwebtoken");
 const adminAuth = require("../authentication/adminAuth");
 const Token = require("../db/models/token_model");
+require("dotenv").config();
 
 route.post("/create", adminAuth, async (req, res) => {
-  const { error } = create(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  const token = req.header("Authorization");
+  const { _id } = jwt.verify(token, process.env.JWT_SECRET);
 
-  const token = new Token(req.body);
+  const user_token = new Token({
+    creatorID: _id,
+    claimerID: "",
+    claimed: false,
+    token: Math.random().toString(36).substring(4),
+  });
 
   try {
-    await token.save();
+    await user_token.save();
     return res.send({ message: "token created" });
   } catch (error) {
     return res
