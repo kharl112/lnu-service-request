215f55afcf67b9401b8fa117d157fb2dc4c3a1a5
kharl112 <kharl.dms112@gmail.com>
2021-05-12 05:50:18 -0500

    claiming token post request in token route created

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,6 +1,8 @@
 const route = require("express").Router();
 const jwt = require("jsonwebtoken");
 const adminAuth = require("../authentication/adminAuth");
+const userAuth = require("../authentication/userAuth");
+const User = require("../db/models/user_model");
 const Token = require("../db/models/token_model");
 require("dotenv").config();
 
@@ -18,6 +20,27 @@ route.post("/create", adminAuth, async (req, res) => {
   try {
     await user_token.save();
     return res.send({ message: "token created" });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "something went wrong, please try again." });
+  }
+});
+
+route.post("/claim", async (req, res) => {
+  const token = req.header("Authorization");
+  const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+
+  try {
+    const user_token = await Token.findOneAndUpdate(
+      { token: req.body.token, claimed: false },
+      { claimed: true, claimerID: _id }
+    );
+
+    if (!user_token)
+      return res.status(400).send({ message: "token not found" });
+
+    return res.send({ message: "token claimed" });
   } catch (error) {
     return res
       .status(500)
