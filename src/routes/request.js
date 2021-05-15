37dbb9c67dae1e4e672b38dca906c52aad69d490
kharl112 <kharl.dms112@gmail.com>
2021-05-15 01:05:33 -0500

    create method for request route created

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,12 +1,42 @@
 const route = require("express").Router();
 const { create } = require("../validation/request_validation");
 const userAuth = require("../authentication/userAuth");
+const User = require("../db/models/user_model");
+const Admin = require("../db/models/admin_model");
+const Request = require("../db/models/request_model");
+const { reset } = require("nodemon");
 
-route.post("/create", userAuth, (req, res) => {
+route.post("/create", userAuth, async (req, res) => {
   const { error } = create(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
-  return res.send(req.body);
+
+  const admin_found = await Admin.findOne({
+    staff_id: req.body.admin.staff_id,
+  });
+  if (!admin_found) return res.status(400).send({ message: "admin not found" });
+
+  if (req.body.head.staff_id) {
+    const head_found = await User.findOne({
+      staff_id: req.body.head.staff_id,
+      department: { unit_role: 2 || 3 },
+    });
+    if (!head_found)
+      return res.status(400).send({ message: "head dept. not found" });
+  }
+
+  const request = new Request({
+    ...req.body,
+    user: { staff_id: req.locals.staff_id, ...req.body.user },
+  });
+
+  try {
+    const new_request = await request.save();
+    return res.send(new_request);
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "something went wrong, please try again." });
+  }
 });
 
 module.exports = route;
