56358f888ea45fbe88c971986143012c1ebbf40d
kharl112 <kharl.dms112@gmail.com>
2021-05-15 05:48:40 -0500

    getAllAdmin and other methods in admin created

--- client/src/store/contents/adminStore.js
+++ client/src/store/contents/adminStore.js
@@ -5,11 +5,14 @@ const admin = {
   namespaced: true,
   state: () => ({
     profile: null,
+    admins: null,
     error: {
       login: null,
+      all_admin: null,
     },
     loading: {
       login: false,
+      all_admin: null,
     },
   }),
   getters: {
@@ -17,14 +20,16 @@ const admin = {
     getError: (state) => state.error,
     getLoading: (state) => state.loading,
     getEmail: (state) => state.email,
+    getAllAdmin: (state) => state.admins,
   },
   mutations: {
     setError: (state, { message, type }) => (state.error[type] = message),
     clearError: (state) => {
-      return (state.error = { login: null, email: null, register: null });
+      return (state.error = { login: null, all_admin: null });
     },
     setLoading: (state, { loading, type }) => (state.loading[type] = loading),
     setProfile: (state, admin_profile) => (state.profile = admin_profile),
+    setAllAdmin: (state, admins) => (state.admins = [...admins]),
   },
   actions: {
     adminLogin: async ({ commit }, form) => {
@@ -52,6 +57,21 @@ const admin = {
         const { message } = error.response.data || error;
         commit("setError", { message, type: "login" });
         return router.replace("/admin/login");
+      }
+    },
+    allAdmin: async ({ commit }) => {
+      commit("clearError");
+      commit("setLoading", { loading: true, type: "all_admin" });
+      try {
+        const { data } = await axios.get("/api/admin/all", {
+          headers: { Authorization: sessionStorage.getItem("Authorization") },
+        });
+        commit("setLoading", { loading: false, type: "all_admin" });
+        return commit("setAllAdmin", data);
+      } catch (error) {
+        const { message } = error.response.data || error;
+        commit("setLoading", { loading: false, type: "all_admin" });
+        return commit("setError", { message, type: "all_admin" });
       }
     },
   },
