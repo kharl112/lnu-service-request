2435c67e7e2a967253314091f0095c226109666d
kharl112 <kharl.dms112@gmail.com>
2021-05-21 07:27:08 -0500

    system testing for production

--- server.js
+++ server.js
@@ -20,5 +20,10 @@ app.use("/api/user", user);
 app.use("/api/token", token);
 app.use("/api/request/", request);
 
+app.use(express.static("client/dist"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
