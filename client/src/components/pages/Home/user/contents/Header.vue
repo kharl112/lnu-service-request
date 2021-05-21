100a68cc42e8e8282a346197d1d5f4be1da7d5a4
kharl112 <kharl.dms112@gmail.com>
2021-05-20 22:22:09 -0500

    badge applied in faculty headers

--- client/src/components/pages/Home/user/contents/Header.vue
+++ client/src/components/pages/Home/user/contents/Header.vue
@@ -21,7 +21,7 @@ export default {
 </script>
 <template>
   <v-container fluid class="home-container3">
-    <v-btn icon color="secondary">
+    <v-btn icon color="secondary" size="30">
       <v-icon>mdi-cached</v-icon>
     </v-btn>
     <v-btn
@@ -30,7 +30,14 @@ export default {
       color="error"
       :disabled="!getSelected[0] && !getLoading.selected"
     >
-      <v-icon>mdi-trash-can</v-icon>
+      <v-badge
+        color="error"
+        offset-x="-18"
+        offset-y="-10"
+        dot
+        v-if="getSelected[0]"
+      />
+      <v-icon size="30">mdi-trash-can</v-icon>
     </v-btn>
   </v-container>
 </template>
