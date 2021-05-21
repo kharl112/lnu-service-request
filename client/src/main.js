f525013bca30654253ba775742117709fbe2a380
kharl112 <kharl.dms112@gmail.com>
2021-05-21 08:05:41 -0500

    Revert "production tip turned on"
    This reverts commit 04114f7b584f5a393437a2ade32267674e0cd983.

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,7 @@ import { routes } from "./routes/routes";
 import store from "./store/store";
 import './registerServiceWorker'
 
-Vue.config.productionTip = true;
+Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 export const router = new VueRouter({
