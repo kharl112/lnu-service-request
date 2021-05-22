072921b8c5faf1627d3d4d771c2ac14dea090fb4
kharl112 <kharl.dms112@gmail.com>
2021-05-22 05:29:27 -0500

    install button applied in landing component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,6 @@
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      deferredPrompt: null,
-    };
-  },
-  created() {
-    window.addEventListener("beforeinstallprompt", (e) => {
-      e.preventDefault();
-      this.deferredPrompt = e;
-    });
-    window.addEventListener("appinstalled", () => {
-      this.deferredPrompt = null;
-    });
-  },
-  methods: {
-    async dismiss() {
-      this.deferredPrompt = null;
-    },
-    async install() {
-      this.deferredPrompt.prompt();
-    },
-  },
 };
 </script>
 
@@ -32,13 +10,6 @@ export default {
       <transition name="fade" mode="out-in">
         <router-view></router-view>
       </transition>
-      <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
-        Get our free app. It won't take up space on your device
-        <template v-slot:actions>
-          <v-btn text @click="dismiss">Dismiss</v-btn>
-          <v-btn text @click="install">Install</v-btn>
-        </template>
-      </v-banner>
     </v-main>
   </v-app>
 </template>
--- client/src/components/pages/Landing/Landing.vue
+++ client/src/components/pages/Landing/Landing.vue
@@ -1,6 +1,28 @@
 <script>
 export default {
   name: "Landing",
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    },
+  },
 };
 </script>
 <template>
@@ -13,5 +35,12 @@ export default {
         <router-link to="/admin/login">Login as admin</router-link>
       </v-col>
     </v-row>
+    <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
+      Get our free app. It won't take up space on your device
+      <template v-slot:actions>
+        <v-btn text @click="dismiss">Dismiss</v-btn>
+        <v-btn text @click="install">Install</v-btn>
+      </template>
+    </v-banner>
   </v-container>
 </template>
