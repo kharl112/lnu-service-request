8059e247b71b2f1e8e10edda7311dbcafc58198b
kharl112 <kharl.dms112@gmail.com>
2021-05-22 08:52:16 -0500

    vue config modified

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,20 @@
- module.exports = {
-   devServer: {
-     proxy: {
-       '/api': {
-         target: 'http://localhost:5000',
-         ws: true,
-         changeOrigin: true
-       }
-     }
-   },
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+  pwa: {
+    name: "LnuSR",
+    themeColor: "#e78a00",
+    msTileColor: "#ffffff",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
 
-   transpileDependencies: [
-     'vuetify'
-   ]
- }
+  transpileDependencies: ["vuetify"],
+};
