06b4ffa59ae0dd0faf85512080f5f2210d269ec9
kharl112 <kharl.dms112@gmail.com>
2021-05-24 19:55:49 -0500

    local error methods in adminStore replaced with root error actions and mutations

--- client/src/store/contents/adminStore.js
+++ client/src/store/contents/adminStore.js
@@ -6,10 +6,6 @@ const admin = {
   state: () => ({
     profile: null,
     admins: null,
-    error: {
-      login: null,
-      all_admin: null,
-    },
     loading: {
       login: false,
       all_admin: false,
@@ -17,23 +13,18 @@ const admin = {
   }),
   getters: {
     getProfile: (state) => state.profile,
-    getError: (state) => state.error,
     getLoading: (state) => state.loading,
     getEmail: (state) => state.email,
     getAllAdmin: (state) => state.admins,
   },
   mutations: {
-    setError: (state, { message, type }) => (state.error[type] = message),
-    clearError: (state) => {
-      return (state.error = { login: null, all_admin: null });
-    },
     setLoading: (state, { loading, type }) => (state.loading[type] = loading),
     setProfile: (state, admin_profile) => (state.profile = admin_profile),
     setAllAdmin: (state, admins) => (state.admins = [...admins]),
   },
   actions: {
-    adminLogin: async ({ commit }, form) => {
-      commit("clearError");
+    adminLogin: async ({ commit, dispatch }, form) => {
+      dispatch("message/defaultState", null, { root: true });
       commit("setLoading", { loading: true, type: "login" });
       commit("setProfile", null);
       try {
@@ -44,10 +35,10 @@ const admin = {
       } catch (error) {
         const { message } = error.response.data || error;
         commit("setLoading", { loading: false, type: "login" });
-        return commit("setError", { message, type: "login" });
+        return dispatch("message/errorMessage", message, { root: true });
       }
     },
-    adminProfile: async ({ commit }) => {
+    adminProfile: async ({ commit, dispatch }) => {
       try {
         const { data } = await axios.get("/api/admin/profile", {
           headers: { Authorization: sessionStorage.getItem("Authorization") },
@@ -55,12 +46,12 @@ const admin = {
         return commit("setProfile", data);
       } catch (error) {
         const { message } = error.response.data || error;
-        commit("setError", { message, type: "login" });
+        dispatch("message/errorMessage", message, { root: true });
         return router.replace("/admin/login");
       }
     },
-    allAdmin: async ({ commit }) => {
-      commit("clearError");
+    allAdmin: async ({ commit, dispatch }) => {
+      dispatch("message/defaultState", null, { root: true });
       commit("setLoading", { loading: true, type: "all_admin" });
       try {
         const { data } = await axios.get("/api/admin/all", {
@@ -71,7 +62,7 @@ const admin = {
       } catch (error) {
         const { message } = error.response.data || error;
         commit("setLoading", { loading: false, type: "all_admin" });
-        return commit("setError", { message, type: "all_admin" });
+        return dispatch("message/errorMessage", message, { root: true });
       }
     },
   },
