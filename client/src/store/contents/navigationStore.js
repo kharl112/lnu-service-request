6463e0bbdc4fa9a32c4692f28d01ab5e204a7621
kharl112 <kharl.dms112@gmail.com>
2021-05-26 22:20:25 -0500

    slide_info state and mutations created in navStore

--- client/src/store/contents/navigationStore.js
+++ client/src/store/contents/navigationStore.js
@@ -1,11 +1,13 @@
 const navigation = {
   namespaced: true,
-  state: () => ({ drawer: true }),
+  state: () => ({ drawer: true, slide_info: false }),
   getters: {
     getDrawer: (state) => state.drawer,
+    getSlideInfo: (state) => state.slide_info,
   },
   mutations: {
     setDrawer: (state, drawer) => (state.drawer = drawer),
+    setSlideInfo: (state, slide_info) => (state.slide_info = slide_info),
   },
 };
 
