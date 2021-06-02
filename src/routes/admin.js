3dd20e1005d0e35fc409a8c36006ffb18d4b3f5b
kharl112 <kharl.dms112@gmail.com>
2021-06-02 04:08:30 -0500

    update route created and fixed create route in admin route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,31 +1,40 @@
 const route = require("express").Router();
 const Admin = require("../db/models/admin_model");
 const adminAuth = require("../authentication/adminAuth");
-const { create, login } = require("../validation/admin_validation");
+const { create, login, update } = require("../validation/admin_validation");
+const { getFixedName } = require("../functions/generateProfile");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const userAuth = require("../authentication/userAuth");
 require("dotenv").config();
 
 route.post("/create", async (req, res) => {
-  const { error } = create(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  const form = { ...req.body };
 
-  const email_found = await Admin.findOne({ email: req.body.email });
+  if (typeof form.name.suffixes !== "object")
+    form.name.suffixes = form.name.suffixes.split(",");
+
+  const { error } = create(form);
+  if (error) return res.status(400).send(error.details[0]);
+
+  form.name.middle_initial = form.name.middle_initial.toUpperCase();
+  form.name.firstname = getFixedName(form.name.firstname);
+  form.name.lastname = getFixedName(form.name.lastname);
+
+  const email_found = await User.findOne({ email: form.email });
   if (email_found)
     return res.status(400).send({ message: "this email already exists." });
 
-  const staff_id_found = await Admin.findOne({ staff_id: req.body.staff_id });
-  if (staff_id_found)
-    return res.status(400).send({ message: "staff_id already exists." });
+  const user_staff_id = await User.findOne({ staff_id: form.staff_id });
+  if (user_staff_id)
+    return res.status(400).send({ message: "staff_id  already exists." });
 
   const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(req.body.password, salt);
+  const hash = bcrypt.hashSync(form.password, salt);
 
   const admin = new Admin({
     ...req.body,
     password: hash,
-    signature: {},
     permitted: false,
   });
 
@@ -35,6 +44,49 @@ route.post("/create", async (req, res) => {
       expiresIn: "7d",
     });
     return res.send({ token });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "something went wrong, please try again." });
+  }
+});
+
+route.post("/update", async (req, res) => {
+  const form = { ...req.body };
+
+  await ["email", "permitted", "password"].map((node) =>
+    form[node] ? delete form[node] : null
+  );
+
+  if (typeof form.name.suffixes !== "object")
+    form.name.suffixes = form.name.suffixes.split(",");
+
+  const { error } = update(form);
+  if (error) return res.status(400).send(error.details[0]);
+
+  form.name.middle_initial = form.name.middle_initial.toUpperCase();
+  form.name.firstname = getFixedName(form.name.firstname);
+  form.name.lastname = getFixedName(form.name.lastname);
+
+  const user_staff_id = await User.findOne({ staff_id: form.staff_id });
+  if (user_staff_id)
+    return res.status(400).send({ message: "staff_id  already exists." });
+
+  try {
+    const updated_admin = await Admin.findOneAndUpdate(
+      {
+        staff_id: req.locals.staff_id,
+        email: { $ne: req.locals.email },
+      },
+      { ...form }
+    );
+
+    if (!updated_admin)
+      return res
+        .status(500)
+        .send({ message: "we can't process your request, please try again." });
+
+    return res.send({ message: "account successfully updated" });
   } catch (error) {
     return res
       .status(500)
