53bff6b47f794430ee794b66c0e832203c2ed205
kharl112 <kharl.dms112@gmail.com>
2021-06-02 03:22:42 -0500

    update validation created in user_validation

--- src/validation/user_validation.js
+++ src/validation/user_validation.js
@@ -29,4 +29,22 @@ const login = (body) => {
   return schema.validate(body);
 };
 
-module.exports = { create, login };
+const update = (body) => {
+  const schema = Joi.object({
+    staff_id: validString.min(7).max(7),
+    name: Joi.object({
+      firstname: validString.min(2).max(255),
+      lastname: validString.min(2).max(255),
+      middle_initial: validString.min(1).max(1),
+      prefix: Joi.string().allow(""),
+      suffixes: Joi.array(),
+    }),
+    department: Joi.object({
+      unit_name: validString.min(2).max(255),
+      unit_role: Joi.number().min(1).max(5),
+    }),
+  });
+  return schema.validate(body);
+};
+
+module.exports = { create, login, update };
