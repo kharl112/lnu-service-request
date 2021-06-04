02189e15906997e7320ad9032c951386fc1c057e
kharl112 <kharl.dms112@gmail.com>
2021-06-04 07:30:49 -0500

    reset password route in user route fixed

--- src/routes/user.js
+++ src/routes/user.js
@@ -165,23 +165,26 @@ route.post("/send/email/link", async (req, res) => {
 route.post("/reset/password/:_id_token", async (req, res) => {
   const { new_password } = req.body;
   const { _id_token } = req.params;
+
   if (!_id_token) return res.status(500).send({ message: "empty parameter" });
 
-  const { _id } = jwt.verify(_id_token, process.env.PASSWORD_RESET);
-
-  const user_found = await User.findById(_id);
-  if (!user_found)
-    return res.status(400).send({ message: "account not found" });
-
-  const salt = bcrypt.genSaltSync(10);
-  const new_hash = bcrypt.hashSync(new_password, salt);
-
   try {
+    const { _id } = jwt.verify(_id_token, process.env.PASSWORD_RESET);
+
+    const user_found = await User.findById(_id);
+    if (!user_found)
+      return res.status(400).send({ message: "account not found" });
+
+    const salt = bcrypt.genSaltSync(10);
+    const new_hash = bcrypt.hashSync(new_password, salt);
+
     user_found.password = new_hash;
     await user_found.save();
+
     const token = jwt.sign({ _id: user_found._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: 120000,
     });
+
     return res.send({ token });
   } catch (error) {
     return res
