1033894bba2aa4cd79fea45c527d60f8ad82fbc3
kharl112 <kharl.dms112@gmail.com>
2021-06-06 23:57:38 -0500

    generateProfile refactored

--- src/functions/generateProfile.js
+++ src/functions/generateProfile.js
@@ -1,40 +1,44 @@
-const getFullName = (name) =>
-  `${name.prefix ? `${name.prefix}.` : ""} ${name.firstname} ${
-    name.middle_initial
-  }. ${name.lastname} ${name.suffixes.toString()}`;
+const Name = (() => {
+  const getFullName = (name) =>
+    `${name.prefix ? `${name.prefix}.` : ""} ${name.firstname} ${
+      name.middle_initial
+    }. ${name.lastname} ${name.suffixes.toString()}`;
 
-const getFixedName = (name) =>
-  name.toLowerCase().replace(/^[a-z]/gi, (first) => first.toUpperCase());
+  const getFixedName = (name) =>
+    name.toLowerCase().replace(/^[a-z]/gi, (first) => first.toUpperCase());
 
-const getFixedFullName = (name) => {
-  if (typeof name.suffixes !== "object")
-    name.suffixes = name.suffixes.split(",");
+  const getFixedFullName = (name) => {
+    if (typeof name.suffixes !== "object")
+      name.suffixes = name.suffixes.split(",");
 
-  name.middle_initial = name.middle_initial.toUpperCase();
-  name.firstname = getFixedName(name.firstname);
-  name.lastname = getFixedName(name.lastname);
+    name.middle_initial = name.middle_initial.toUpperCase();
+    name.firstname = getFixedName(name.firstname);
+    name.lastname = getFixedName(name.lastname);
 
-  return name;
-};
+    return name;
+  };
 
-const getDepartment = ({ unit_role, unit_name }) =>
-  `${unit_name} ${unit_role === 1 ? "" : "Head"} Department`;
+  return { getFullName, getFixedName, getFixedFullName };
+})();
 
-const getFullDate = (date) => {
-  const new_date = new Date(date);
-  return `${
-    new_date.getMonth() + 1 < 10
-      ? `0${new_date.getMonth() + 1}`
-      : new_date.getMonth() + 1
-  }/${
-    new_date.getDate() < 10 ? `0${new_date.getDate()}` : new_date.getDate()
-  }/${new_date.getFullYear()}`;
-};
+const Department = (() => {
+  const getDepartment = ({ unit_role, unit_name }) =>
+    `${unit_name} ${unit_role === 1 ? "" : "Head"} Department`;
+  return { getDepartment };
+})();
 
-module.exports = {
-  getFullName,
-  getDepartment,
-  getFixedName,
-  getFullDate,
-  getFixedFullName,
-};
+const Date = (() => {
+  const getFullDate = (date) => {
+    const new_date = new Date(date);
+    return `${
+      new_date.getMonth() + 1 < 10
+        ? `0${new_date.getMonth() + 1}`
+        : new_date.getMonth() + 1
+    }/${
+      new_date.getDate() < 10 ? `0${new_date.getDate()}` : new_date.getDate()
+    }/${new_date.getFullYear()}`;
+  };
+  return { getFullDate };
+})();
+
+module.exports = { Name, Date, Department };
