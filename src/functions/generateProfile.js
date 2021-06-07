15563322df3a3431f56ffb8efe9ca5793183e2a6
kharl112 <kharl.dms112@gmail.com>
2021-06-06 23:52:38 -0500

    getFixedFullName created in generateProfile Function

--- src/functions/generateProfile.js
+++ src/functions/generateProfile.js
@@ -3,11 +3,22 @@ const getFullName = (name) =>
     name.middle_initial
   }. ${name.lastname} ${name.suffixes.toString()}`;
 
-const getDepartment = ({ unit_role, unit_name }) =>
-  `${unit_name} ${unit_role === 1 ? "" : "Head"} Department`;
-
 const getFixedName = (name) =>
   name.toLowerCase().replace(/^[a-z]/gi, (first) => first.toUpperCase());
+
+const getFixedFullName = (name) => {
+  if (typeof name.suffixes !== "object")
+    name.suffixes = name.suffixes.split(",");
+
+  name.middle_initial = name.middle_initial.toUpperCase();
+  name.firstname = getFixedName(name.firstname);
+  name.lastname = getFixedName(name.lastname);
+
+  return name;
+};
+
+const getDepartment = ({ unit_role, unit_name }) =>
+  `${unit_name} ${unit_role === 1 ? "" : "Head"} Department`;
 
 const getFullDate = (date) => {
   const new_date = new Date(date);
@@ -20,4 +31,10 @@ const getFullDate = (date) => {
   }/${new_date.getFullYear()}`;
 };
 
-module.exports = { getFullName, getDepartment, getFixedName, getFullDate };
+module.exports = {
+  getFullName,
+  getDepartment,
+  getFixedName,
+  getFullDate,
+  getFixedFullName,
+};
