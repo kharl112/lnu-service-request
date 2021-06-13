c83b60d403185e034a5626934d1fb97bfe209d8f
kharl112 <kharl.dms112@gmail.com>
2021-06-13 05:24:15 -0500

    serviceStore created

---
+++ client/src/store/contents/serviceStore.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const services = {
+  namespaced: true,
+  state: () => ({
+    all_services: [],
+    loading: false,
+  }),
+  getters: {
+    getAllServices: (state) => state.all_services,
+    getLoading: (state) => state.loading,
+  },
+  mutations: {
+    setAllServices: (state, all_services) =>
+      (state.all_services = [...all_services]),
+    setLoading: (state, loading) => (state.loading = loading),
+  },
+  actions: {
+    allServices: async ({ commit, dispatch }) => {
+      commit("setLoading", true);
+      try {
+        const { data } = await axios.get("/api/service/all", {
+          headers: { Authorization: localStorage.getItem("Authorization") },
+        });
+        commit("setLoading", false);
+        return commit("setAllServices", data);
+      } catch (error) {
+        const { message } = error.response.data || error;
+        commit("setLoading", false);
+        return dispatch("message/errorMessage", message, { root: true });
+      }
+    },
+  },
+};
+
+export default services;
