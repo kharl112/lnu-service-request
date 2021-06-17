3f490724dc8e0a9eeda1235f42bc9fb2c62105c8
kharl112 <kharl.dms112@gmail.com>
2021-06-16 23:41:42 -0500

    generateToken updated'

--- src/functions/generateToken.js
+++ src/functions/generateToken.js
@@ -1,15 +1,15 @@
 const Token = require("../db/models/token_model");
 
-module.exports = (token_found) => {
+module.exports = (token_found, staff_id) => {
   if (token_found) {
     token_found.token = Math.random().toString(36).substring(1, 10);
-    token_found.claimer_staff_id = req.locals.staff_id;
+    token_found.claimer_staff_id = staff_id;
 
     await token_found.save();
     return token_found;
   } else {
     const user_token = new Token({
-      claimer_staff_id: new_user.staff_id,
+      claimer_staff_id: staff_id,
       token: Math.random().toString(36).substring(1, 10),
     });
     await user_token.save();
