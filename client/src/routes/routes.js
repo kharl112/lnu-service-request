5e1e61227c6acb8e5b48577a8caf89a2d6fbde2a
kharl112 <kharl.dms112@gmail.com>
2021-06-25 02:55:01 -0500

    AdminArchives imported and applied in routes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -29,6 +29,7 @@ import HomeAdmin from "../components/pages/Home/admin/HomeAdmin";
 import AdminSigned from "../components/pages/Home/admin/children/AdminSigned";
 import AdminPending from "../components/pages/Home/admin/children/AdminPending";
 import AdminSettings from "../components/pages/Home/admin/children/AdminSettings";
+import AdminArchives from "../components/pages/Home/admin/children/AdminArchives";
 
 import store from "../store/store";
 import axios from "axios";
@@ -194,6 +195,7 @@ export const routes = [
       { path: "settings", component: AdminSettings },
       { path: "pending", component: AdminPending },
       { path: "signed", component: AdminSigned },
+      { path: "archives", component: AdminArchives },
     ],
   },
 ];
