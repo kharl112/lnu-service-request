e09a598b1a7fb2605481521f48e25abc0c2286ff
kharl112 <kharl.dms112@gmail.com>
2021-07-12 22:27:36 -0500

    AdminPending updated: set in tabular form

--- client/src/components/pages/Home/admin/children/AdminPending.vue
+++ client/src/components/pages/Home/admin/children/AdminPending.vue
@@ -8,6 +8,7 @@ export default {
   },
   data: () => ({
     signatureVisibility: false,
+    selected: "",
     selectedRequest: "",
     colors: ["primary", "warning", "error", "success"],
   }),
@@ -48,6 +49,7 @@ export default {
       });
     },
     downloadPDF(id) {
+      this.selected = id;
       return this.$store.dispatch("pdf/generatePDF", {
         user_type: "admin",
         id,
@@ -75,141 +77,136 @@ export default {
 <template>
   <v-container fluid class="pa-0 pa-sm-3">
     <v-row dense justify="start" v-if="!getProfileLoading">
-      <v-col cols="12" sm="12" md="8">
+      <v-col cols="12">
+        <v-container fluid class="pr-0 pb-0 pt-0">
+          <v-col cols="8" sm="5" md="4">
+            <span class="h4 primary--text"> Pending Requests </span>
+          </v-col>
+          <v-divider />
+        </v-container>
         <v-container fluid v-if="getAllPending[0] && !getLoading.all_pending">
-          <v-expansion-panels accordion hover>
-            <v-expansion-panel
-              outlined
-              v-for="pending in getAllPending"
-              :key="pending.name"
-            >
-              <v-expansion-panel-header>
-                <div align="left" class="hidden-sm-and-down pr-3">
-                  <v-avatar size="40" :color="getRandomColor()">
-                    <span class="white--text text-subtitle-1">{{
-                      getInitials(pending.user.profile[0].name)
-                    }}</span>
-                  </v-avatar>
-                </div>
-                <v-container fluid>
-                  <v-row>
+          <v-simple-table fixed-header>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    Description
+                  </th>
+                  <th class="text-center">
+                    Type
+                  </th>
+                  <th class="text-center">
+                    Created
+                  </th>
+                  <th class="text-center">
+                    Status
+                  </th>
+                  <th class="text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="pending in getAllPending" :key="pending.name">
+                  <td>
                     <v-list-item-subtitle
-                      align="left"
-                      class="pa-0 text-left text-body-1 "
+                      @click="$router.push(`/track/${pending._id}`)"
+                      class="pa-0 text-caption text-left text-sm-body-2 text-lowercase"
                     >
                       {{ pending.subject }}
                     </v-list-item-subtitle>
-                    <v-card-subtitle
-                      class="pa-0 text-subtitle-2 text-no-wrap primary--text hidden-md-and-up"
+                  </td>
+                  <td class="text-center">
+                    <v-list-item-subtitle
+                      @click="$router.push(`/track/${pending._id}`)"
+                      class="pa-0 text-caption text-sm-body-2 text-lowercase"
                     >
                       {{ pending.service[0].type }}
-                      <span class="text-caption ">
-                        &#40;
-                        {{ getTimeOrDate(pending.date) }}
-                        &#41;
-                      </span>
-                    </v-card-subtitle>
-                  </v-row>
-                </v-container>
-                <div align="right" class="hidden-sm-and-down">
-                  <v-chip
-                    small
-                    max-width="70px"
-                    class="ma-2 text-center pr-2 pl-2 caption"
-                    color="primary"
+                    </v-list-item-subtitle>
+                  </td>
+                  <td
+                    class="text-center"
+                    @click="$router.push(`/track/${pending._id}`)"
                   >
-                    {{ getTimeOrDate(pending.date) }}
-                  </v-chip>
-                </div>
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <v-divider class="mb-2" />
-                <v-card-subtitle
-                  class="pa-0 pb-2 text-caption text-justify text-sm-body-2"
-                >
-                  {{ pending.body }}
-                </v-card-subtitle>
-                <v-divider />
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" class="pa-1 pl-0">
-                      <v-card-subtitle class="pa-0 text-caption ">
-                        <span class="font-weight-bold">FROM: </span>
-                        {{ getFullname(pending.user.profile[0].name) }}
-                        <span class="hidden-sm-and-down font-italic">
-                          -
-                          {{
-                            `${pending.user.department.role[0].name} of ${pending.user.department.unit[0].name}`
-                          }}
-                        </span>
-                      </v-card-subtitle>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      class="pa-1 pl-0"
-                      v-if="pending.service_provider.profile[0]"
+                    <v-chip
+                      small
+                      color="primary"
+                      class="pa-0 pr-2 pl-2 text-center text-caption"
                     >
-                      <v-card-subtitle class="pa-0 text-caption ">
-                        <span class="font-weight-bold">TO: </span>
-                        {{
-                          getFullname(pending.service_provider.profile[0].name)
-                        }}
-                        <span class="hidden-sm-and-down font-italic">
-                          {{
-                            `${pending.service_provider.department.role[0].name} of ${pending.service_provider.department.unit[0].name}`
-                          }}
-                        </span>
-                      </v-card-subtitle>
-                    </v-col>
-                  </v-row>
-                </v-container>
-                <v-divider />
-                <v-card-actions class="pa-3 pt-3 pb-3">
-                  <v-row justify="start" align="center">
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="3"
-                      align="start"
-                      class="pa-2 pl-0"
+                      {{ getTimeOrDate(pending.date) }}
+                    </v-chip>
+                  </td>
+                  <td
+                    class="text-center"
+                    @click="$router.push(`/track/${pending._id}`)"
+                  >
+                    <small
+                      :class="
+                        `pa-0 text-caption font-weight-bold ${
+                          pending.status === 0
+                            ? 'primary--text'
+                            : 'success--text'
+                        }`
+                      "
                     >
-                      <v-btn
-                        block
-                        min-width="50px"
-                        color="success"
-                        @click="showSignature(pending._id)"
-                      >
-                        Sign
-                        <v-icon right>
-                          mdi-signature-freehand
-                        </v-icon>
-                      </v-btn>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="3"
-                      align="start"
-                      class="pa-2 pl-0"
-                    >
-                      <v-btn
-                        block
-                        min-width="50px"
-                        color="error"
-                        :disabled="getPDFLoading"
-                        @click="downloadPDF(pending._id)"
-                      >
-                        Download
-                        <v-icon right>
-                          mdi-cloud-download
-                        </v-icon>
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </v-card-actions>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
+                      {{ pending.status === 0 ? "Pending" : "Completed" }}
+                    </small>
+                  </td>
+                  <td class="text-center text-no-wrap">
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          icon
+                          v-bind="attrs"
+                          v-on="on"
+                          class="mt-2 mb-1"
+                          color="error"
+                          :loading="getPDFLoading && selected === pending._id"
+                          @click="downloadPDF(pending._id)"
+                        >
+                          <v-icon>
+                            mdi-cloud-download
+                          </v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Download</span>
+                    </v-tooltip>
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          icon
+                          v-bind="attrs"
+                          v-on="on"
+                          class="mt-2 mb-1"
+                          color="success"
+                          @click="showSignature(pending._id)"
+                        >
+                          <v-icon>
+                            mdi-signature-freehand
+                          </v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Sign request</span>
+                    </v-tooltip>
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          icon
+                          v-bind="attrs"
+                          v-on="on"
+                          class="mt-2 mb-1"
+                          color="primary"
+                        >
+                          <v-icon>
+                            mdi-eye
+                          </v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Preview request</span>
+                    </v-tooltip>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
         </v-container>
         <v-container fluid v-else-if="getLoading.all_pending">
           <v-skeleton-loader type="table" />
@@ -217,104 +214,18 @@ export default {
         <v-container
           fluid
           v-else-if="!getAllPending[0] && !getLoading.all_pending"
-          ><v-row justify="start">
+        >
+          <v-row justify="start">
             <v-col cols="12">
               <v-banner single-line>
                 <v-icon slot="icon" color="warning" size="36">
                   mdi-exclamation-thick
                 </v-icon>
-                No pending request
+                You have empty unsigned requests
               </v-banner>
             </v-col>
           </v-row>
         </v-container>
-      </v-col>
-      <v-divider class="hidden-sm-and-down" vertical />
-      <v-col md="4" class="hidden-sm-and-down">
-        <v-row>
-          <v-col cols="12">
-            <v-container fluid>
-              <v-card>
-                <v-img
-                  src="https://anlantawan.files.wordpress.com/2020/08/ellenyu.jpg?w=720"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  width="720px"
-                  height="auto"
-                >
-                  <v-card-title>
-                    Leyte Normal University
-                  </v-card-title>
-                  <v-subheader class="white--text">
-                    Pamantasang Normal ng Leyte
-                  </v-subheader>
-                </v-img>
-                <v-card-text>
-                  <v-row align="center" class="mx-0">
-                    <v-rating
-                      :value="4.2"
-                      color="amber"
-                      dense
-                      half-increments
-                      readonly
-                      size="14"
-                    />
-                    <div class="grey--text ms-4">
-                      4.2 (81 reviews)
-                    </div>
-                  </v-row>
-                  <div class="my-4 subtitle-1">
-                    Public University
-                  </div>
-                </v-card-text>
-                <v-divider class="mx-4"></v-divider>
-                <v-card-actions>
-                  <v-list dense>
-                    <v-row justify="start" align="start">
-                      <v-col cols="12" class="pa-2 pt-0 pb-0">
-                        <v-list-item>
-                          <v-icon color="primary">
-                            mdi-google-maps
-                          </v-icon>
-                          <v-subheader>
-                            <a
-                              href="https://www.google.com/maps/place/Leyte+Normal+University/@11.2380362,124.9995256,17z/data=!4m9!1m2!2m1!1slnu!3m5!1s0x3308772c87c4c367:0xa5e5c080ec6a88ef!8m2!3d11.2381879!4d125.001328!15sCgNsbnWSARFwdWJsaWNfdW5pdmVyc2l0eQ"
-                              color="primary"
-                            >
-                              Paterno St, Downtown, Tacloban City, 6500 Leyte
-                            </a>
-                          </v-subheader>
-                        </v-list-item>
-                      </v-col>
-                      <v-col cols="12" class="pa-2 pt-0 pb-0">
-                        <v-list-item>
-                          <v-icon color="primary">
-                            mdi-web
-                          </v-icon>
-                          <v-subheader>
-                            <a href="http://www.lnu.edu.ph/" color="primary">
-                              lnu.edu.ph
-                            </a>
-                          </v-subheader>
-                        </v-list-item>
-                      </v-col>
-                      <v-col cols="12" class="pa-2 pt-0 pb-0">
-                        <v-list-item>
-                          <v-icon color="primary">
-                            mdi-phone
-                          </v-icon>
-                          <v-subheader>
-                            +63538880855
-                          </v-subheader>
-                        </v-list-item>
-                      </v-col>
-                    </v-row>
-                  </v-list>
-                </v-card-actions>
-              </v-card>
-            </v-container>
-          </v-col>
-        </v-row>
       </v-col>
     </v-row>
     <v-row
