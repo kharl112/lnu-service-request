54000cad329e07b3d74b57401e63f0e0728c19c6
kharl112 <kharl.dms112@gmail.com>
2021-07-21 02:28:13 -0500

    Risograph component created

---
+++ client/src/components/pages/Home/user/children/options/Risograph.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "Risograph",
+  props: {
+    options: Object,
+    rules: Array,
+  },
+};
+</script>
+<template>
+  <v-col cols="12" sm="12" md="4">
+    <v-row justify="start" align="start">
+      <v-col cols="12">
+        <v-container fluid class="pt-0 pb-0">
+          <v-subheader>Job Order for Risograph Options</v-subheader>
+          <v-divider />
+        </v-container>
+      </v-col>
+      <v-col cols="12">
+        <v-card outlined class="mx-auto">
+          <v-list-item>
+            <v-list-item-content> </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
