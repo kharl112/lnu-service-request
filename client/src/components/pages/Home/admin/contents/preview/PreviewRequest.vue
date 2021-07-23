783d5a65df85e6e0a3f295f500457b4854a516c9
kharl112 <kharl.dms112@gmail.com>
2021-07-22 22:54:01 -0500

    PreviewRequest applied in admin

--- client/src/components/pages/Home/admin/contents/preview/PreviewRequest.vue
+++ client/src/components/pages/Home/admin/contents/preview/PreviewRequest.vue
@@ -1,6 +1,11 @@
 <script>
+import Original from "./contents/Original";
+import Mailing from "./contents/Mailing";
+import PassSlip from "./contents/PassSlip";
+import Risograph from "./contents/Risograph";
 export default {
   name: "PreviewRequest",
+  components: { Original, Mailing, PassSlip, Risograph },
   props: {
     preview: Object,
     showPreview: Function,
@@ -11,25 +16,10 @@ export default {
       return this.$store.getters["pdf/getLoading"];
     },
   },
-  methods: {
-    fixDate(date) {
-      const new_date = new Date(date);
-      return `${new_date.getMonth()}/${new_date.getDate()}/${new_date.getFullYear()}`;
-    },
-    getFullname(name) {
-      const { firstname, lastname, middle_initial, prefix, suffixes } = name;
-      return `${
-        prefix ? `${prefix}.` : ""
-      } ${firstname} ${middle_initial.toUpperCase()}. ${lastname} ${suffixes.toString()}`;
-    },
-    getDepartment({ role, unit }) {
-      return `${role[0].name} of ${unit[0].name}`;
-    },
-  },
 };
 </script>
 <template>
-  <v-overlay :value="preview.show" :z-index="100">
+  <v-overlay :value="preview.show" :z-index="100" class="overlay">
     <v-sheet
       v-if="preview.data"
       elevation="1"
@@ -37,8 +27,9 @@ export default {
       width="95vw"
       height="99vh"
       color="white"
+      class="pa-3 sheet"
     >
-      <v-container fluid>
+      <v-container fluid class="inner-sheet pb-10">
         <v-row>
           <v-col cols="12" class="pa-6">
             <v-row justify="end">
@@ -55,175 +46,29 @@ export default {
               </v-btn>
             </v-row>
           </v-col>
-          <v-col cols="12">
-            <v-container fluid class="pa-2">
-              <v-row justify="center">
-                <v-img
-                  src="https://upload.wikimedia.org/wikipedia/en/1/17/LNUTaclobanLogo.jpg"
-                  max-width="75"
-                  min-width="25"
-                  class="mr-2 hidden-sm-and-down"
-                />
-                <v-col cols="12" md="6">
-                  <v-row justify="center">
-                    <v-col cols="12" class="ml-2">
-                      <v-row justify="center" justify-md="start">
-                        <span class="black--text text-center body-1">
-                          Republic of the Philippines
-                        </span>
-                      </v-row>
-                    </v-col>
-                    <v-col cols="12" class="ml-2">
-                      <v-row justify="center" justify-md="start">
-                        <span class="black--text font-weight-bold body-2">
-                          Leyte Normal University
-                        </span>
-                      </v-row>
-                    </v-col>
-                    <v-col cols="12" class="ml-2">
-                      <v-row justify="center" justify-md="start">
-                        <span class="black--text caption">
-                          P. Paterno St., Tacloban City
-                        </span>
-                      </v-row>
-                    </v-col>
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col cols="12" class="mt-0 pb-0">
-            <v-container fluid>
-              <v-row justify="center">
-                <span
-                  class="black--text text-capitalize font-weight-bold body-1"
-                >
-                  {{ preview.data.subject }}
-                </span>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col cols="12">
-            <v-container fluid class="pl-7">
-              <v-row justify="start">
-                <v-col cols="12" class="pa-0">
-                  <span class="black--text caption">
-                    Service type: {{ preview.data.service[0].type }}
-                  </span>
-                </v-col>
-                <v-col cols="12" class="pa-0">
-                  <span class="black--text caption">
-                    Date created: {{ fixDate(preview.data.date) }}
-                  </span>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col cols="12" class="mt-0 pb-0">
-            <v-container fluid class="pt-0 pb-2">
-              <v-row>
-                <v-col cols="12" class="pt-0 pb-0">
-                  <p class="black--text text-justify caption">
-                    {{ preview.data.body }}
-                  </p>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col cols="12" class="mt-5">
-            <v-container fluid class="pt-0 pb-2">
-              <v-row>
-                <v-col cols="12" class="pt-0 pb-0">
-                  <small
-                    class="black--text text-capitalize caption text-decoration-underline"
-                  >
-                    From: {{ getFullname(preview.data.user.profile[0].name) }}
-                    <v-icon
-                      v-if="preview.data.user.signature"
-                      small
-                      color="success"
-                      class="ml-1 mb-1"
-                    >
-                      mdi-check-circle
-                    </v-icon>
-                  </small>
-                </v-col>
-                <v-col cols="12" class="pt-0 pb-0 mt-n2">
-                  <small class="black--text caption tex-center">
-                    {{ getDepartment(preview.data.user.department) }}
-                  </small>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col
-            cols="12"
-            class="mt-0"
-            v-if="preview.data.service_provider.staff_id"
-          >
-            <v-container fluid class="pt-0 pb-2">
-              <v-row>
-                <v-col cols="12" class="pt-0 pb-0">
-                  <small
-                    class="black--text text-capitalize caption text-decoration-underline"
-                  >
-                    To:
-                    {{
-                      getFullname(preview.data.service_provider.profile[0].name)
-                    }}
-                    <v-icon
-                      v-if="preview.data.service_provider.signature"
-                      small
-                      color="success"
-                      class="ml-1 mb-1"
-                    >
-                      mdi-check-circle
-                    </v-icon>
-                  </small>
-                </v-col>
-                <v-col cols="12" class="pt-0 pb-0 mt-n2">
-                  <small class="black--text caption tex-center">
-                    {{
-                      getDepartment(preview.data.service_provider.department)
-                    }}
-                  </small>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col cols="12" class="mt-0">
-            <v-container fluid class="pt-0 pb-2">
-              <v-row>
-                <v-col cols="12" class="pt-0 pb-0">
-                  <small
-                    class="black--text text-capitalize caption text-decoration-underline"
-                  >
-                    {{ getFullname(preview.data.admin.profile[0].name) }}
-                    <v-icon
-                      v-if="preview.data.admin.signature"
-                      small
-                      color="success"
-                      class="ml-1 mb-1"
-                    >
-                      mdi-check-circle
-                    </v-icon>
-                  </small>
-                </v-col>
-                <v-col cols="12" class="pt-0 pb-0 mt-n2">
-                  <small class="black--text caption tex-center">
-                    Chief Administration Office
-                  </small>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
+          <Mailing
+            :preview="preview"
+            v-if="preview.data.service_id === '60f62de769f7dd1017e2ba4b'"
+          />
+          <Risograph
+            :preview="preview"
+            v-else-if="preview.data.service_id === '60f62dd969f7dd1017e2ba4a'"
+          />
+          <PassSlip
+            :preview="preview"
+            v-else-if="preview.data.service_id === '60f62dcb69f7dd1017e2ba49'"
+          />
+          <Original v-else :preview="preview" />
         </v-row>
       </v-container>
     </v-sheet>
   </v-overlay>
 </template>
 <style scoped>
-.pdf-sheet {
+.inner-sheet {
+  border: 2px solid black;
+}
+.sheet {
   overflow-y: scroll;
 }
 </style>
---
+++ client/src/components/pages/Home/admin/contents/preview/contents/PassSlip.vue
@@ -0,0 +1,215 @@
+<script>
+export default {
+  name: "PassSlip",
+  props: {
+    preview: Object,
+  },
+  methods: {
+    fixDate(date) {
+      const new_date = new Date(date);
+      return `   ${new_date.getMonth()}/${new_date.getDate()}/${new_date.getFullYear()}   `;
+    },
+    getFullname(name) {
+      const { firstname, lastname, middle_initial, prefix, suffixes } = name;
+      return `${
+        prefix ? `${prefix}.` : ""
+      } ${firstname} ${middle_initial.toUpperCase()}. ${lastname} ${suffixes.toString()}`;
+    },
+    getDepartment({ role, unit }) {
+      return `${role[0].name} of ${unit[0].name}`;
+    },
+  },
+};
+</script>
+<template>
+  <v-col cols="12" class="pa-0 mt-n7 content">
+    <v-col cols="12">
+      <v-container fluid class="pa-2">
+        <v-row justify="center">
+          <v-col cols="12" md="6">
+            <v-row justify="center">
+              <v-col cols="12" class="ml-2">
+                <v-row justify="center">
+                  <span class="black--text text-center caption">
+                    Republic of the Philippines
+                  </span>
+                </v-row>
+              </v-col>
+              <v-col cols="12" class="ml-2">
+                <v-row justify="center">
+                  <span
+                    class="black--text text-center subtitle-2 text-uppercase"
+                  >
+                    Leyte Normal University
+                  </span>
+                </v-row>
+              </v-col>
+              <v-col cols="12" class="ml-2">
+                <v-row justify="center">
+                  <span class="black--text text-center caption">
+                    Tacloban City
+                  </span>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12" class="mt-0 pt-0 pb-0">
+      <v-container fluid>
+        <v-row justify="center">
+          <span
+            class="black--text text-uppercase text-h6 text-decoration-underline"
+          >
+            Pass Slip
+          </span>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12">
+      <v-container fluid class="pr-10">
+        <v-row justify="end">
+          <span class="black--text text-center body-2">
+            Date:
+            <span class="text-decoration-underline text-center">
+              {{ fixDate(preview.data.date) }}
+            </span>
+          </span>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12">
+      <v-container fluid class="pt-0">
+        <v-row justify="start">
+          <v-col cols="12" class="pa-0">
+            <span class="black--text font-weight-bold body-2">
+              NAME:
+              <span class="ml-1 font-weight-regular text-decoration-underline">
+                {{ preview.data.options.name }}
+              </span>
+            </span>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12" class="pt-1">
+      <v-container fluid class="pt-0">
+        <v-row justify="start">
+          <v-col cols="12" class="pa-0">
+            <span class="black--text font-weight-bold body-2">
+              TIMEOUT:
+              <span class="ml-1 font-weight-regular text-decoration-underline">
+                {{ preview.data.options.timeout }}
+              </span>
+            </span>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12" class="pt-1">
+      <v-container fluid class="pt-0">
+        <v-row justify="start">
+          <v-col cols="12" class="pa-0">
+            <span class="black--text font-weight-bold body-2">
+              PURPOSE:
+              <span class="ml-1 font-weight-regular text-decoration-underline">
+                {{ preview.data.body }}
+              </span>
+            </span>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12" class="pl-0 pt-2">
+      <v-container fluid class="mt-2 pb-2">
+        <v-row>
+          <v-col cols="12">
+            <v-row justify="end">
+              <span class="black--text text-uppercase text-center caption">
+                <span class="text-decoration-underline text-center">
+                  {{ getFullname(preview.data.user.profile[0].name) }}
+                  <v-icon
+                    v-if="preview.data.user.signature"
+                    small
+                    color="success"
+                    class="ml-1 mb-1"
+                  >
+                    mdi-check-circle
+                  </v-icon>
+                </span>
+                <v-spacer />
+                <small class="black--text caption text-center text-capitalize">
+                  (signature over printed name)
+                </small>
+              </span>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12" v-if="preview.data.service_provider.staff_id" class="pl-0">
+      <v-container fluid class="mt-2 pt-0 pb-2">
+        <v-row>
+          <v-col cols="12" class="pt-0 pb-0">
+            <span class="black--text text-uppercase caption ">
+              JOB DONE BY:
+              <v-spacer />
+              <span class="text-decoration-underline text-center">
+                {{ getFullname(preview.data.service_provider.profile[0].name) }}
+                <v-icon
+                  v-if="preview.data.service_provider.signature"
+                  small
+                  color="success"
+                  class="ml-1 mb-1"
+                >
+                  mdi-check-circle
+                </v-icon>
+              </span>
+            </span>
+          </v-col>
+          <v-col cols="12" class="pt-0 pb-0 mt-n2">
+            <small class="black--text caption text-center text-capitalize">
+              {{ getDepartment(preview.data.service_provider.department) }}
+            </small>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+    <v-col cols="12">
+      <span class="text-left black--text caption pb-0 pt-0"> APPROVED BY: </span>
+    </v-col>
+    <v-col cols="12">
+      <v-container fluid class="pt-0 pb-2">
+        <v-row>
+          <v-col cols="12" class="pt-0 pb-0">
+            <v-row justify="start">
+              <span class="black--text text-uppercase text-center caption ">
+                <span class="text-decoration-underline text-center">
+                  {{ getFullname(preview.data.admin.profile[0].name) }}
+                  <v-icon
+                    v-if="preview.data.admin.signature"
+                    small
+                    color="success"
+                    class="ml-1 mb-1"
+                  >
+                    mdi-check-circle
+                  </v-icon>
+                </span>
+                <v-spacer />
+                <small class="black--text caption text-center text-capitalize">
+                  Chief Administrative Officer-Admin
+                </small>
+              </span>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+  </v-col>
+</template>
+<style scoped>
+.footer {
+  border-top: 2px dashed black;
+}
+</style>
