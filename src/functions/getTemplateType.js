7dfafe9361bc7d7691be24260a034919dfdbb912
kharl <kharl.dms112@gmail.com>
2021-09-15 23:11:21 -0500

    removed parent obj 'form' in getTemplateType

--- src/functions/getTemplateType.js
+++ src/functions/getTemplateType.js
@@ -1,9 +1,9 @@
 module.exports = (service_id) => {
   return service_id.toString() === "60f62de769f7dd1017e2ba4b"
     ? "mailing"
-    : form.service_id.toString() === "60f62dcb69f7dd1017e2ba49"
+    : service_id.toString() === "60f62dcb69f7dd1017e2ba49"
     ? "passslip"
-    : form.service_id.toString() === "60f62dd969f7dd1017e2ba4a"
+    : service_id.toString() === "60f62dd969f7dd1017e2ba4a"
     ? "risograph"
     : "original";
 };
