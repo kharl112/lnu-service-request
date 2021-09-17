68bf828ddff30905151ff319c2b569c82bdbf734
kharl <kharl.dms112@gmail.com>
2021-09-17 02:54:00 -0500

    docOptions created

---
+++ client/src/components/pages/Home/user/children/options/docOptions.js
@@ -0,0 +1,21 @@
+const form = {
+  subject: "",
+  service_id: "",
+  body: "",
+  options: {},
+  user: {
+    signature: "",
+  },
+  admin: {
+    staff_id: "",
+    signature: "",
+  },
+  service_provider: {
+    staff_id: "",
+    signature: "",
+  },
+  save_as: 0,
+  other_service: "",
+};
+
+export default form;
