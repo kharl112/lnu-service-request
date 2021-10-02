894f0da793eacd14fe1cbf37227efba79b106262
kharl <kharl.dms112@gmail.com>
2021-10-01 22:28:22 -0500

    slider side navigation state fixed

--- client/src/components/pages/Home/admin/contents/SideNav.vue
+++ client/src/components/pages/Home/admin/contents/SideNav.vue
@@ -58,7 +58,7 @@ export default {
       },
     },
   },
-  mounted() {
+  destroyed() {
     this.drawer = false;
   },
 };
--- client/src/components/pages/Home/user/contents/SideNav.vue
+++ client/src/components/pages/Home/user/contents/SideNav.vue
@@ -78,7 +78,7 @@ export default {
       },
     },
   },
-  mounted() {
+  destroyed() {
     this.drawer = false;
   },
 };
