4e054b4e0397ddb187756b341d25615b28b8be2e
kharl <kharl.dms112@gmail.com>
2021-10-28 06:59:24 -0500

    view/request and not/found route applied in admin routes

--- client/src/routes/admin/admin_routes.js
+++ client/src/routes/admin/admin_routes.js
@@ -3,6 +3,8 @@ import AdminSigned from "../../components/pages/Home/admin/children/AdminSigned"
 import AdminPending from "../../components/pages/Home/admin/children/AdminPending";
 import AdminSettings from "../../components/pages/Home/admin/children/AdminSettings";
 import AdminArchives from "../../components/pages/Home/admin/children/AdminArchives";
+import ViewRequest from "../../components/pages/Home/admin/children/ViewRequest";
+import NotFound from "../../components/pages/Home/admin/children/NotFound";
 
 import store from "../../store/store";
 
@@ -18,5 +20,20 @@ export const admin_routes = {
     { path: "pending", component: AdminPending },
     { path: "signed", component: AdminSigned },
     { path: "archives", component: AdminArchives },
+    { path: "error/404", component: NotFound },
+    {
+      path: "view/:id",
+      component: ViewRequest,
+      beforeEnter: async (to, from, next) => {
+        await store.dispatch("request/viewRequest", {
+          id: to.params.id,
+          user_type: "admin",
+        });
+
+        if (store.getters["request/getLetterInfo"]) {
+          next();
+        } else next("/admin/home/error/404");
+      },
+    },
   ],
 };
