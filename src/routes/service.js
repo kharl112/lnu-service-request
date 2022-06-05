54ba70eeb84243122936d182e376c804cabeb9f3
kharl112 <kharl.dms112@gmail.com>
2022-06-05 07:35:00 -0500

    valid services applied in all route in service routes

--- src/routes/service.js
+++ src/routes/service.js
@@ -3,9 +3,23 @@ const Service = require("../db/models/service_model");
 
 route.get("/all", async (req, res) => {
   try {
-    const all_services = await Service.find({});
+    const valid_services = [
+      "60f62dcb69f7dd1017e2ba49",
+      "60f62dd969f7dd1017e2ba4a",
+      "60f62e6169f7dd1017e2ba51",
+      "629c8b230db34e279582a537"
+    ];
+
+    const all_services = await Service.find({
+      $or: [
+        ...valid_services.map((id) => ({
+          _id: id,
+        })),
+      ],
+    });
     return res.send(all_services);
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .send({ message: "something went wrong please try again" });
