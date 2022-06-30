a032663dcd4730def87ce6b9b41172a8cf4df66b
kharl112 <kharl.dms112@gmail.com>
2022-06-30 07:54:07 -0500

    upload progress state, getter and mutation created in gdriveStore

--- client/src/store/contents/gdriveStore.js
+++ client/src/store/contents/gdriveStore.js
@@ -2,15 +2,18 @@ import axios from "axios";
 const gdrive = {
   namespaced: true,
   state: () => ({
+    upload_percent: 0,
     loading: {
       upload: false,
     },
   }),
   getters: {
     getLoading: (state) => state.loading,
+    getUploadPercent: (state) => state.upload_percent,
   },
   mutations: {
     setLoading: (state, { type, loading }) => (state.loading[type] = loading),
+    setUploadPercent: (state, percent) => (state.upload_percent = percent),
   },
   actions: {
     UploadFile: async (
@@ -19,6 +22,8 @@ const gdrive = {
     ) => {
       dispatch("message/defaultState", null, { root: true });
       commit("setLoading", { type: "upload", loading: true });
+      commit("setUploadPercent", 0);
+
       try {
         const formData = new FormData(form_ref);
         formData.append("file", file);
@@ -31,8 +36,15 @@ const gdrive = {
             "Content-Type": "multipart/form-data",
             Authorization: localStorage.getItem("Authorization"),
           },
+          onUploadProgress: (event) => {
+            const { loaded, total } = event;
+            commit("setUploadPercent", Math.floor((loaded * 100) / total));
+          },
         });
+
         commit("setLoading", { type: "upload", loading: false });
+        commit("setUploadPercent", 0);
+
         dispatch(
           "request/Info",
           {
@@ -46,6 +58,8 @@ const gdrive = {
         });
       } catch (error) {
         commit("setLoading", { type: "upload", loading: false });
+        commit("setUploadPercent", 0);
+
         dispatch(
           "message/errorMessage",
           "something went wrong, pls try again",
