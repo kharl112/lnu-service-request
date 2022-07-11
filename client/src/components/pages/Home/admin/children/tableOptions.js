2f4e3d8518b4b2ad81136001c1fc21c6cd57e2cc
kharl112 <kharl.dms112@gmail.com>
2022-07-11 05:50:42 -0500

    tableOptions in admin items rearranged

--- client/src/components/pages/Home/admin/children/tableOptions.js
+++ client/src/components/pages/Home/admin/children/tableOptions.js
@@ -2,28 +2,28 @@ const tableOptions = (value, text) => ({
   search: "",
   headers: [
     {
+      text: "Service Type",
+      align: "left",
+      sortable: true,
+      value: "service[0].type",
+    },
+    {
+      text: "Description",
+      align: "left",
+      sortable: true,
+      value: "subject",
+    },
+    {
       text: "Requestor Name",
       align: "left",
       sortable: true,
       value: "user.profile",
     },
     {
-      text: "Position/Role",
-      align: "left",
-      sortable: true,
-      value: "user.department.role",
-    },
-    {
       text: "Department",
       align: "left",
       sortable: true,
       value: "user.department.unit",
-    },
-    {
-      text: "Service Type",
-      align: "left",
-      sortable: true,
-      value: "service[0].type",
     },
     {
       text,
