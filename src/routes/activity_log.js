c4de9597f5583d3c0f31c01b728c5b14538283d6
kharl112 <kharl.dms112@gmail.com>
2022-07-22 21:49:07 -0500

    activityLogQuery function applied in activity_log route functions

--- src/routes/activity_log.js
+++ src/routes/activity_log.js
@@ -8,6 +8,7 @@ const ActivityLog = require("../db/models/activity_log_model");
 //middlewares
 const userAuth = require("../middlewares/userAuth");
 const adminAuth = require("../middlewares/adminAuth");
+const activityLogQuery = require("../functions/activityLogQuery");
 
 route.get("/faculty", userAuth, async (req, res) => {
   try {
@@ -19,9 +20,13 @@ route.get("/faculty", userAuth, async (req, res) => {
     if (!owner) return res.status(400).send({ message: "user not found" });
 
     //find activity logs and send back to client owner
-    const activity_log = await ActivityLog.find({ "user.staff_id": staff_id });
+    const activity_log = await ActivityLog.aggregate(
+      activityLogQuery({ "user.staff_id": staff_id })
+    );
+
     res.send(activity_log);
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .send({ message: "something went wrong, please try again" });
@@ -41,10 +46,13 @@ route.get("/admin", adminAuth, async (req, res) => {
         .send({ message: "cao personnel account not found" });
 
     //find activity logs and send back to client owner
-    const activity_log = await ActivityLog.find({
-      "user.staff_id": staff_id,
-      "user.user_type": "admin",
-    });
+    const activity_log = await ActivityLog.aggregate(
+      activityLogQuery({
+        "user.staff_id": staff_id,
+        "user.user_type": "admin",
+      })
+    );
+
     res.send(activity_log);
   } catch (error) {
     return res
