017301b0d45c5c718237fb6240296e63753fd42c
kharl112 <kharl.dms112@gmail.com>
2022-07-24 08:17:16 -0500

    Notificaiton component created in user and applied in user_routes

--- client/src/routes/user/user_routes.js
+++ client/src/routes/user/user_routes.js
@@ -11,6 +11,7 @@ import ViewRequest from "../../components/pages/Home/user/children/ViewRequest";
 import NotFound from "../../components/pages/Home/user/children/NotFound";
 import FacultyDashboard from "../../components/pages/Home/user/children/FacultyDashboard";
 import ActivityLogs from "../../components/pages/Home/user/children/ActivityLogs";
+import Notifications from "../../components/pages/Home/user/children/Notifications";
 
 import store from "../../store/store";
 
@@ -30,6 +31,7 @@ export const user_routes = {
   children: [
     { path: "dashboard", component: FacultyDashboard },
     { path: "activity-log", component: ActivityLogs },
+    { path: "notification", component: Notifications },
     { path: "drafts", component: Drafts },
     { path: "sent", component: Sent },
     { path: "compose", component: Compose },
